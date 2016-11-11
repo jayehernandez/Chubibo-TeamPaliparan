d1e1a8690a54cf302f8d4a268419214d5c7fd617
Jaye Hernandez <hernandez.jaye@gmail.com>
2016-11-12 02:02:02 +0800

    Hi ysa

--- teamPaliparan/teamPaliparan/views.py
+++ teamPaliparan/teamPaliparan/views.py
@@ -1,4 +1,9 @@
-from django.http import HttpResponse  
+from django.template.loader import get_template
+from django.template import Context
+from django.http import HttpResponse
+import datetime
 
-def hello(request):
-    return HttpResponse("Hello world")
+def hwllo(request):
+    t = get_template('hello.html')
+    html = t.render()
+    return HttpResponse(html)
