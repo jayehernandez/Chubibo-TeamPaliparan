30d70fcd319352b21b38248dd9cd87a1b4deb331
Jaye Hernandez <hernandez.jaye@gmail.com>
2016-11-12 02:03:35 +0800

    Hi ysa

--- teamPaliparan/teamPaliparan/views.py
+++ teamPaliparan/teamPaliparan/views.py
@@ -3,7 +3,7 @@ from django.template import Context
 from django.http import HttpResponse
 import datetime
 
-def hwllo(request):
+def hello(request):
     t = get_template('hello.html')
     html = t.render()
     return HttpResponse(html)
